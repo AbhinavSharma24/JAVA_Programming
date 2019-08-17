7cbafbc9c641dc01964c6486b81bd984c6c1e1c1
abhinav <abhinavsharma24999@gmail.com>
2019-08-18 00:32:39 +0530

    Initial Commit

--- src/LAB2/This.java
+++ src/LAB2/This.java
@@ -2,25 +2,27 @@ package LAB2;
 
 import java.util.Scanner;
 
-public class BoxCons {
+public class This {
     int width,height,depth;
-    BoxCons(){
-        width=0;height=0;depth=0;
+    This(int width,int height,int depth){
+        this.width=width;
+        this.height=height;
+        this.depth=depth;
     }
     void volume(){
         System.out.println("Volume : "+width*height*depth);
     }
     public static void main(String[] args) {
 
-        BoxCons b1 = new BoxCons();
+        This b1 = new This(2,4,6);
         Scanner s  = new Scanner(System.in);
-        System.out.println("Volume of non parametrised constructor : ");
+        System.out.println("Volume of parametrised constructor : ");
         b1.volume();
-        System.out.println("Enter the width,height and depth of Box 1 : ");
+
+        System.out.println("Enter the width,height and depth of Box  : ");
         b1.width=s.nextInt();
         b1.height=s.nextInt();
         b1.depth=s.nextInt();
         b1.volume();
-
     }
 }
