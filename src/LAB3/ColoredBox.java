0736ce208cfa12f39409913a3302c67b93c741ff
abhinav <abhinavsharma24999@gmail.com>
2019-08-23 01:06:28 +0530

    Initial Commit

---
+++ src/LAB3/ColoredBox.java
@@ -0,0 +1,32 @@
+package LAB3;
+
+import java.util.Scanner;
+
+class Box {
+    float l,w,h;
+    static Scanner s = new Scanner(System.in);
+    void get() {
+        System.out.println("Enter l,w,h : ");
+        l = s.nextFloat();
+        w = s.nextFloat();
+        h = s.nextFloat();
+    }
+    void print() {
+        System.out.println("Volume : "+l*w*h);
+    }
+}
+
+public class ColoredBox extends Box{
+    static  String color;
+    void print() {
+        super.print();
+        System.out.println(color);
+    }
+    public static void main(String[] args) {
+        ColoredBox coloredBox = new ColoredBox();
+        System.out.println("Enter color : ");
+        color = s.next();
+        coloredBox.get();
+        coloredBox.print();
+    }
+}
