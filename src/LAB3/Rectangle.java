327bd9d32975a921f911dcbb4a78c07fb0dd390c
abhinav <abhinavsharma24999@gmail.com>
2019-08-23 01:06:59 +0530

    Initial Commit

---
+++ src/LAB3/Rectangle.java
@@ -0,0 +1,38 @@
+package LAB3;
+
+import java.util.Scanner;
+
+class Shape
+{
+    double d1,d2;
+    Scanner input=new Scanner(System.in);
+
+    void get()
+    {
+        d1=input.nextInt();
+        d2=input.nextInt();
+    }
+    void print()
+    {
+
+    }
+}
+class Rectangle extends Shape
+{
+    @Override
+    void print()
+    {
+        System.out.println("Length ="+d1);
+        System.out.println("Breadth ="+d2);
+    }
+
+    public static void main(String[] args) {
+        Rectangle rectangle=new Rectangle();
+
+        System.out.println("Enter length and breadth of rectangle ");
+
+        rectangle.get();
+        rectangle.print();
+    }
+
+}
