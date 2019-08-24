e8c2cf06ef241466cda9eb0cfa2e23c0b02746a1
abhinav <abhinavsharma24999@gmail.com>
2019-08-24 18:15:16 +0530

    Initial Commit

---
+++ src/LAB4/dividedByZero.java
@@ -0,0 +1,21 @@
+package LAB4;
+
+import java.util.Scanner;
+public class dividedByZero {
+    public static void main(String[] args) {
+        int a=5;
+        int b=0;
+        int c;
+        try{
+            Scanner s=new Scanner(System.in);
+            System.out.println("enter value of A : ");
+            a = s.nextInt();
+            System.out.println("Enter value of B : ");
+            b = s.nextInt();
+            System.out.println("Answer : " + a/b);
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
