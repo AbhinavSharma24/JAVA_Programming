1b144ec8a2330c7fa40f023c05a391bb198ed429
abhinav <abhinavsharma24999@gmail.com>
2019-09-08 01:03:00 +0530

    Initial Commit

---
+++ src/LAB5/ModifyFont.java
@@ -0,0 +1,21 @@
+package LAB5;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class ModifyFont extends Applet {
+    private Font f1;
+    public void init(){
+        f1 = new Font("Comic Sans MS",Font.ITALIC | Font.BOLD, 35);
+    }
+    public void paint(Graphics g){
+        g.setFont(f1);
+        g.setColor(Color.black);
+        g.drawString("Hello Abhinav, Font Modified.", 70, 70);
+    }
+}
+
+/*
+<applet code="FirstApplet.class" height=300 width=300>
+</applet>
+*/
