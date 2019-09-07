373f21f5c1923581a7bfaf0ab1309bb3e378cdb9
abhinav <abhinavsharma24999@gmail.com>
2019-09-08 01:02:29 +0530

    Initial Commit

--- src/LAB5/LifecycleMethods.java
+++ src/LAB5/LifecycleMethods.java
@@ -1,4 +1,30 @@
 package LAB5;
 
-public class LifecycleMethods {
+import java.applet.Applet;
+import java.awt.*;
+
+public class LifecycleMethods extends Applet {
+    public void init() {
+        setBackground(Color.CYAN);
+        System.out.println("init() called");
+    }
+    public void start() {
+        System.out.println("start() called");
+    }
+    public void paint(Graphics g) {
+        g.setFont(new Font("Arial", Font.BOLD, 20));
+        g.drawString("Life-Cycle methods called.", 70, 70);
+        System.out.println("paint() called");
+    }
+    public void stop() {
+        System.out.println("stop() Called");
+    }
+    public void destroy() {
+        System.out.println("destroy)() Called");
+    }
 }
+
+/*
+<applet code="LifecycleMethods.class" width=350 height=150>
+</applet>
+*/
