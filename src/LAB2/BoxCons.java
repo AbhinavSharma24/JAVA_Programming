5ce0e3e72a0639e1627fe51d5249d9a50fc64eac
abhinav <abhinavsharma24999@gmail.com>
2019-10-08 22:54:10 +0530

    Initial Commit

--- src/LAB2/BoxCons.java
+++ src/LAB2/BoxCons.java
@@ -3,20 +3,20 @@ package LAB2;
 import java.util.Scanner;
 
 public class BoxCons {
-    int width,height,depth;
-    BoxCons(){
+    private int width,height,depth;
+    private BoxCons(){
         width=0;height=0;depth=0;
     }
-    void volume(){
+    private void volume(){
         System.out.println("Volume : "+width*height*depth);
     }
     public static void main(String[] args) {
 
         BoxCons b1 = new BoxCons();
         Scanner s  = new Scanner(System.in);
-        System.out.println("Volume of non parametrised constructor : ");
+        System.out.println("Volume of non parametrised constructor :: ");
         b1.volume();
-        System.out.println("Enter the width, height and depth of Box 1 : ");
+        System.out.println("Enter the width, height and depth of Box 1 :: ");
         b1.width=s.nextInt();
         b1.height=s.nextInt();
         b1.depth=s.nextInt();
--- src/LAB6/FirstThread.java
+++ src/LAB6/FirstThread.java
@@ -2,6 +2,5 @@ package LAB6;
 
 public class FirstThread {
     //first thread program yet to come
-    //will share soon XD
     //don't worry :)
 }
