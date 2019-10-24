7e6e8996963c65c650f23971d516b1a0a3a93353
abhinav <abhinavsharma24999@gmail.com>
2019-10-24 20:24:15 +0530

    Initial Commit

---
+++ src/Thread_LAB/StringFile.java
@@ -0,0 +1,17 @@
+package Thread_LAB;
+
+public class StringFile {
+    public static void main(String[] args) {
+        String st=new String("Abhinav");
+        System.out.println(st.charAt(4));
+        System.out.println(st.equals("Abhinav"));
+        System.out.println(st.toUpperCase());
+        System.out.println(st.concat("Sharma"));
+        System.out.println(st.contains("a"));
+        System.out.println(st.indexOf('s'));
+        System.out.println(st.replace('h','H'));
+        st=st.toLowerCase();
+        System.out.println(st);
+        System.out.println(st.length());
+    }
+}
